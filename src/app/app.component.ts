bd91b41eb29b3e07aa4a00edd76e7d48e6a59b2d
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2023-12-11 15:56:05 +0300

    Add example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface City {
+  name: string,
+  code: string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'primeng-multiselect-placeholder-error';
+export class AppComponent implements OnInit {
+  cities!: City[];
+  staticSelectedCities!: City[];
+  dynamicSelectedCities!: City[];
+
+  ngOnInit() {
+    this.cities = [
+      {name: 'New York', code: 'NY'},
+      {name: 'Rome', code: 'RM'},
+      {name: 'London', code: 'LDN'},
+      {name: 'Istanbul', code: 'IST'},
+      {name: 'Paris', code: 'PRS'}
+    ];
+  }
 }
